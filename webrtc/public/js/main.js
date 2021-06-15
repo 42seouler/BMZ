b93fa6988a0a92876c79a9147e6eee0cfd5788d6
42seouler <42seouler@gmail.com>
2021-06-15 20:44:37 +0900

    클라이언트 소켓 url 변경

--- webrtc/public/js/main.js
+++ webrtc/public/js/main.js
@@ -9,7 +9,7 @@ import {getState} from "./store.js";
 
 // console.log(window.screen.width, window.screen.height);
 // initialization of socketIO connection
-const socket = io("/");
+const socket = io("https://randommeal.du.r.appspot.com/");
 wss.registerSocketEvents(socket);
 
 webRTCHandler.getLocalPreview();
